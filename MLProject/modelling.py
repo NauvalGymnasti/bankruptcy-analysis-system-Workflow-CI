0362e3e7c3549a3dea441afa0a5cc8dbb7cfadaf
nauvalgymnasti7 <nauvalgymnasti7@gmail.com>
2025-11-24 05:33:09 +0700

    run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,7 +9,15 @@ from Company_Bankruptcy_Prediction_data_preprocessing.preprocessing import load_
 import pandas as pd
 
 # --- 1. Load Data ---
-X_train, X_test, y_train, y_test = load_and_preprocess_data()
+data_path = "data_celan.csv"
+data = pd.read_csv(data_path)
+
+X = data.drop("Bankrupt?", axis=1)
+y = data["Bankrupt?"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 # --- 2. Konfigurasi MLflow ---
 mlflow.set_tracking_uri("http://127.0.0.1:5000") 
